66e7f9b8f69588f1e74c2e4d0d836af61b3b44ce
joten300 <joten300@yahoo.com>
2023-07-14 07:01:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio>
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
- * Return 1 (success)
+ * main - printing without printf and puts
+ *
+ * Return: always (1)
+ *
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
