843c359029e7b6c3936b92b6aa3547a1f9b2ce83
joten300 <joten300@yahoo.com>
2023-07-28 13:51:28 -0700

    strnpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,9 +2,9 @@
 
 /**
  * _strncpy - copy a string
- * @dest - input value
- * @src - input value
- * @n - input value
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
