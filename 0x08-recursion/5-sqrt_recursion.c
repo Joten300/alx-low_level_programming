ce9fc66379d35cf11684e264a520875347812f98
joten300 <joten300@yahoo.com>
2023-08-03 23:08:59 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,6 +5,7 @@ int actual_sqrt_recursion(int n, int i);
  * _sqrt_recursion - a function that returns the natural
  * square root of a number
  * @n: square root number
+ *
  * Return: the square root result
  */
 int _sqrt_recursion(int n)
@@ -24,7 +25,7 @@ int _sqrt_recursion(int n)
  */
 int actual_sqrt_recursion(int n, int i)
 {
-	if (i * i < n)
+	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
